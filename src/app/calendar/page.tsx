cf4ef90ac9e7f42b0b44b010517d91f422b7e4e0
onfranciis <onukwuf@gmail.com>
2026-04-05 14:54:02 +0100

    fix: rename VolunteerPage to CalendarPage and add SpacePage component

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-export default function VolunteerPage() {
+export default function CalendarPage() {
   redirect(
     "https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NjI5YWk0b29lbzZ0dG9rdTN1MjNyc29mN2UgOTQ5ZTlmYzExOThmNmJhYmEyOTA4YjJmY2FmNzY4MzgyN2MyMTBkMWNjYzdhMTE2OWJjOWQwYzU0ZWVhNDUxY0Bn&tmsrc=949e9fc1198f6baba2908b2fcaf7683827c210d1ccc7a1169bc9d0c54eea451c%40group.calendar.google.com",
   );
